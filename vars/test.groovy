31d2f8e9d68a444bf92436dde3220eae2d4eb40d
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-02-20 22:32:29 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,4 @@
 def call(){
 
-  sh 'mvn test /Users/krvnbangarraju/Desktop/Merchants/target/classes/com/example/*class'
+  sh 'mvn test /Users/krvnbangarraju/Desktop/Merchants/target/classes/com/example/'
 }
