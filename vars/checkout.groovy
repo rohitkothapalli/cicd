5e54a6f3ac27b613435262be44175516a8d07f37
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-02-21 00:02:32 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,16 @@
-def call(){
-
-  git url: "https://github.com/rohitkothapalli/javaapp"
-  
+def call() {
+  node {
+    stage('Checkout') {
+      // Checkout your source code from Git or another SCM
+      git url: 'https://github.com/rohitkothapalli/javaapp'
+    }
+    stage('Build') {
+      // Build the JAR file using Maven
+      sh 'mvn clean package'
+    }
+    stage('Archive') {
+      // Archive the JAR file as an artifact
+      archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+    }
+  }
 }
