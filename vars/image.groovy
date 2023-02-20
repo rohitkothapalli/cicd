cef8d033f962fae9cba22393d71d6c7f152209b3
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-02-21 00:00:43 +0530

    Delete image.groovy

--- vars/image.groovy
+++
@@ -1,7 +0,0 @@
-def call(){
-
-    sh 'docker login -u 'krvnb' -p 'RohiT.123''
-    sh 'docker build -t Dockerfile /Users/krvnbangarraju/Desktop/Merchants/target/classes/com/example/'
-    
-}
-    
