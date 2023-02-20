82686168b5dbde0ec88fa5c0533c815bb0089afe
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-02-20 23:00:34 +0530

    Update image.groovy

--- vars/image.groovy
+++ vars/image.groovy
@@ -1,11 +1,7 @@
-def call(String project, String hubUser) {
-    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
-    withCredentials([usernamePassword(
-            credentialsId: "docker",
-            usernameVariable: "krvnb",
-            passwordVariable: "RohiT.123"
-    )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
-    }
-    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+def call(){
+
+    sh 'docker login -u 'krvnb' -p 'RohiT.123''
+    sh 'docker build -t Dockerfile /Users/krvnbangarraju/Desktop/Merchants/target/classes/com/example/'
+    
 }
+    
