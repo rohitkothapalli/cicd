b2b6e48469f35fa7943a687a6707bb7d49b8acea
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-02-21 18:35:56 +0530

    Update dockerimage.groovy

--- vars/dockerimage.groovy
+++ vars/dockerimage.groovy
@@ -3,6 +3,6 @@ def VERSION = '1.0.0'
 def call(){
   
   sh 'cd /var/jenkins_home/workspace/task/target'
-  sh 'docker build -f krvnb/taskapp dockerfile .'
+  sh 'docker build -t dockerfile .'
   
 }
