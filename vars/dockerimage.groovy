ba879fe197cbeeb11532073f2ac000d695c0614f
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-02-21 15:24:00 +0530

    Update dockerimage.groovy

--- vars/dockerimage.groovy
+++ vars/dockerimage.groovy
@@ -1,6 +1,8 @@
 def VERSION = '1.0.0'
 def dockerImageName= 'krvnb/app_$JOB_NAME:$BUILD_NUMBER'
 def call(){
+  sh 'dockerd'
+  sh 'docker login -u 'krvnb' -p 'RohiT.123''
   sh 'cd /var/jenkins_home/workspace/task/target'
   sh 'docker build -t {dockerImageName} .'
   
