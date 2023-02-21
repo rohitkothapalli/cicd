8c5378bbfed4cb7c9ba219260674d59a8bde1780
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-02-21 17:58:19 +0530

    Update publishimage.groovy

--- vars/publishimage.groovy
+++ vars/publishimage.groovy
@@ -1,6 +1,12 @@
 def VERSION = '1.0.0'
 
+def dockerImageName= 'krvnb/app_$JOB_NAME:$BUILD_NUMBER'
+
 def call(){
+  
+         withCredentials([string(credentialsId: 'krvnb', variable: 'dockerPWD')]) {
+              sh "docker login -u krvnb -p RohiT.123"
+         }
  
         sh "docker push ${dockerImageName}"
 }
